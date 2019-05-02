3dae1225fd3bae974a2305fd9ec725d1f51875ea
Chuma Okoro <chuma.okoro76@gmail.com>
2019-05-02 16:23:38 -0400

    fix prime number php code

--- php/prime.php
+++ php/prime.php
@@ -1,13 +1,26 @@
 <?php
-    echo "Hello. Welcome to the prime number determiner";
-    if($_GET['number'] % 3 == 5){
-        echo " ";
+    echo "<h3>Hello. Welcome to the prime number determiner</h3>";
+    if(is_numeric($_GET["number"]) && $_GET["number"] > 0 && $_GET["number"] == round($_GET["number"], 0)){
+        $i=2;
+        $prime=true;
+        while($i<$_GET["number"]){
+            if($_GET['number'] % $i == 0){
+                $prime=false;
+            }
+            $i++;
+        }
+
+        if($prime){
+            echo "<p>The number ".$_GET['number']." is prime!</p>" ;
+        }
+        else{
+            echo "<p>The number ".$_GET['number']." is not prime!</p>" ;
+        }
     }
+    else if($_GET){
 
-
-
-    echo "The number ".$_GET['number']."is not prime!" ;
-
+        echo "<p>Please enter a positive whole number. The value ".$_GET["number"]." is not valid.";
+    }
 ?>
 
 <p>Enter a number</p>
