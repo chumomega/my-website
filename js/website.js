ebfeac3a2f2452796fb242754cc77164beacdcf4
Chuma Okoro <chuma.okoro76@gmail.com>
2019-06-06 17:51:09 -0400

    backup from cuny tech prep laptop

--- js/website.js
+++ js/website.js
@@ -1,23 +1,23 @@
 //the function getdropdown makes the dropdown visible when you click on the button
 
 $(function(){
-    $("#nav-bar-container").load("../html/header.html");
+    $("nav").load("../html/header.html");
 });
 
 $(function(){
     $("#footer-container").load("../html/footer.html");    
 });
 
-function getDropdown(){
-    document.getElementById("dropdown-group").classList.toggle("show");
-}
+// function getDropdown(){
+//     document.getElementById("dropdown-group").classList.toggle("show");
+// }
 
-window.onclick = function(event){
-    if(!event.target.matches("#nav-button")){
-        var dropdown = document.getElementById("dropdown-group");
+// window.onclick = function(event){
+//     if(!event.target.matches("#nav-button")){
+//         var dropdown = document.getElementById("dropdown-group");
     
-        if(dropdown.classList.contains('show')){
-            dropdown.classList.remove('show');
-        }
-    }
-}
+//         if(dropdown.classList.contains('show')){
+//             dropdown.classList.remove('show');
+//         }
+//     }
+// }
